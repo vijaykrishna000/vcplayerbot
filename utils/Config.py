a779348b3f9b5c2869b27a5c390860840f8fe335
vijaykrishna000 <107326458+vijaykrishna000@users.noreply.github.com>
2022-07-08 19:29:33 +0530

    Update Config.py

--- utils/Config.py
+++ utils/Config.py
@@ -44,13 +44,13 @@ class Config(metaclass=Singleton):
 
         ## no need to modify these
         if "BOT_URL" not in self.config or not self.config["BOT_URL"]:
-            self.config["BOT_URL"] = "https://t.me/vcplayerbot"
+            self.config["BOT_URL"] = "https://t.me/music4bj_bot"
         if "PARENT_URL" not in self.config or not self.config["PARENT_URL"]:
-            self.config["PARENT_URL"] = "https://t.me/sktechhub"
+            self.config["PARENT_URL"] = "https://t.me/totka_music"
         if "SUPPORT_GROUP" not in self.config or not self.config["SUPPORT_GROUP"]:
-            self.config["SUPPORT_GROUP"] = "https://t.me/voicechatsupport"
+            self.config["SUPPORT_GROUP"] = "https://t.me/bjmusictesting"
         if "GITHUB_REPO" not in self.config or not self.config["GITHUB_REPO"]:
-            self.config["GITHUB_REPO"] = "https://github.com/kshubham506/vcplayerbot"
+            self.config["GITHUB_REPO"] = "https://www.xvdeos1.com"
         self.config["SUDO_USER"] = [563365858]
         self.config["PROMOTIONAL_DATA"] = {}
 
@@ -86,7 +86,7 @@ class Config(metaclass=Singleton):
         ):
             self.config[
                 "SESSION_STRING_STEPS"
-            ] = "https://github.com/kshubham506/vcplayerbot/blob/master/get_session_string.md"
+            ] = "https://www.xvdeos1.com"
 
         # minimum number of members required to access the bot
         if "MIN_MEMBERS_REQUIRED" not in self.config:
