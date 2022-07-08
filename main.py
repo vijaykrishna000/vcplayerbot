2da42314a92266f09042d9dfe918a4bb1c264fcf
vijaykrishna000 <107326458+vijaykrishna000@users.noreply.github.com>
2022-07-08 22:26:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def main():
 if __name__ == "__main__":
     try:
         logInfo(
-            "Started the Backend Server For Voice Chat Music Player | BJ Music Bot"
+            "Started the Backend Server For Music Player | BJ Music Bot"
         )
         main()
     except Exception as ex:
